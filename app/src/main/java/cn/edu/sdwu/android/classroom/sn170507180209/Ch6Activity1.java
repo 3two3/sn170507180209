038dacd81aa4283dc50db5c7085775d47288980a
coffee <87863496@qq.com>
2020-04-08 16:14:38 +0800

    2020-04-08课堂练习

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180209/Ch6Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180209/Ch6Activity1.java
@@ -1,8 +1,18 @@
 package cn.edu.sdwu.android.classroom.sn170507180209;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Ch6Activity1 extends AppCompatActivity {
 
@@ -18,5 +28,76 @@ public class Ch6Activity1 extends AppCompatActivity {
         String sms=getString(R.string.sms);
         String s = String.format(sms, 100, "张三");
         Log.i(Ch6Activity1.this.toString(),s);
+
+        Resources resources = getResources();
+
+        int[] intArray = resources.getIntArray(R.array.intArr);
+        for(int i=0;i<intArray.length;i++){
+            Log.i(Ch6Activity1.this.toString(),intArray[i]+"");
+        }
+
+        String[] stringArray = resources.getStringArray(R.array.strArr);
+        for(int i=0;i<stringArray.length;i++){
+            Log.i(Ch6Activity1.this.toString(),stringArray[i]);
+        }
+
+        TypedArray typedArray = resources.obtainTypedArray(R.array.commanArr);
+        ImageView imageView = (ImageView)findViewById(R.id.ch6_1_iv);
+        int resourceId = typedArray.getResourceId(0, 0);
+        imageView.setImageResource(resourceId);
+
+        String str = typedArray.getString(1);
+        Log.i(Ch6Activity1.this.toString(),str);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ch6_1_ll);
+        registerForContextMenu(linearLayout);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.mymenu1,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mymenu_item1:
+                Toast.makeText(this,"item1",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mymenu_item2:
+                Toast.makeText(this,"item2",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mymenu_item3:
+                Toast.makeText(this,"item3",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //基于回调的事件处理
+        //根据菜单项的ID进行区分
+        switch (item.getItemId()){
+            case R.id.mymenu_item1:
+                Toast.makeText(this,"item1",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mymenu_item2:
+                Toast.makeText(this,"item2",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mymenu_item3:
+                Toast.makeText(this,"item3",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.mymenu1,menu);
+        return true;
     }
 }
