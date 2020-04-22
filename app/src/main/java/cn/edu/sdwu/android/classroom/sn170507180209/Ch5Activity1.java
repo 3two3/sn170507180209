3d1ff587d2735271740d74e2103ad6d6f6cf47dd
coffee <87863496@qq.com>
2020-04-22 21:46:53 +0800

    2020-04-22课堂练习

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180209/Ch5Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180209/Ch5Activity1.java
@@ -2,7 +2,9 @@ package cn.edu.sdwu.android.classroom.sn170507180209;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -12,8 +14,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static android.widget.Toast.makeText;
-
 public class Ch5Activity1 extends AppCompatActivity {
 
     @Override
@@ -22,73 +22,81 @@ public class Ch5Activity1 extends AppCompatActivity {
         setContentView(R.layout.layout_ch5_1);
     }
 
-    public void toast1(View view){
-        makeText(this,"toast1",Toast.LENGTH_LONG).show();
+    public void toast1(View view) {
+        Toast.makeText(this, "toast1", Toast.LENGTH_LONG).show();
     }
 
-    public void toast2(View view){
-        Toast toast = Toast.makeText(this,"toast2",Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER,0,0);
+    public void toast2(View view) {
+        Toast toast = Toast.makeText(this, "toast2", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
 
-    public void toast3(View view){
-        //加载自定义布局文件，将其转换为一个View类的实例
+    public void toast3(View view) {
+        //加载自定义的布局文件，将其转换为一个View类的实例
         LayoutInflater layoutInflater = getLayoutInflater();
-        View view1 = layoutInflater.inflate(R.layout.layout_toast,null);
+        View view1 = layoutInflater.inflate(R.layout.layout_toast, null);
 
-        //设置文本
-        TextView textView = (TextView)view1.findViewById(R.id.toast_tv);
+        //设置显示的文本
+        TextView textView = (TextView) view1.findViewById(R.id.toast_tv);
         textView.setText("toast3");
+
         Toast toast = new Toast(this);
-        toast.setView(view1);
-        toast.setDuration(Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.CENTER,0,0);
+        toast.setView(view1);//设置界面
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
 
-    public void notification(View view){
+    public void notification(View view) {
         Notification.Builder builder = new Notification.Builder(this);
-        //在构造器中，完成各种通知的属性的设置
+        //在构造器中，完成通知各种属性的设置
         builder.setContentTitle("title");
         builder.setContentText("message");
         builder.setWhen(System.currentTimeMillis());
-        builder.setSmallIcon(android.R.drawable.ic_input_delete);//引用系统资源
-        //调用build方法，完成构造
+        builder.setSmallIcon(android.R.drawable.ic_input_delete);//引用系统的资源
+
+        //2020.4.22
+        Intent intent = new Intent(this, Ch6Activity1.class);//点击通知之后，跳转的界面
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 101, intent, PendingIntent.FLAG_CANCEL_CURRENT);//这个类代表代办的Intent
+        builder.setContentIntent(pendingIntent);
+
+        //调用build方法 完成构造
         Notification notification = builder.build();
-        //使用NotificationManager,发送通知
-        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(101,notification);
+        //使用NotificationManager，发送通知
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(101, notification);
     }
 
-    public void cancel_notification(View view){
-        //使用NotificationManager,取消通知
-        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+    public void cancel_notification(View view) {
+        //使用NotificationManager取消通知
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.cancel(101);
     }
 
-    public void alert_dialog(View view){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    public void alert_dialog(View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //在构造器中设置对话框属性
         builder.setTitle("title");
         builder.setMessage("message");
-        builder.setIcon(android.R.drawable.ic_dialog_alert);
-        builder.setPositiveButton("confirm", new DialogInterface.OnClickListener(){
-
+        builder.setIcon(android.R.drawable.ic_dialog_email);
+        builder.setPositiveButton("confirm", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(Ch5Activity1.this,"confirm",Toast.LENGTH_LONG).show();
+                //添加点击按钮后的处理
+                Toast.makeText(Ch5Activity1.this, "confirm", Toast.LENGTH_SHORT).show();
             }
         });
-        builder.setNegativeButton("exit",new DialogInterface.OnClickListener(){
+        builder.setNegativeButton("exit", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(Ch5Activity1.this,"exit",Toast.LENGTH_LONG).show();
+                Toast.makeText(Ch5Activity1.this, "exit", Toast.LENGTH_SHORT).show();
             }
         });
-        builder.setNeutralButton("cancel",new DialogInterface.OnClickListener(){
+        builder.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(Ch5Activity1.this,"cancel",Toast.LENGTH_LONG).show();
+                Toast.makeText(Ch5Activity1.this, "cancel", Toast.LENGTH_SHORT).show();
             }
         });
         builder.create().show();
--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180209/Ch6Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180209/Ch6Activity1.java
@@ -1,5 +1,6 @@
 package cn.edu.sdwu.android.classroom.sn170507180209;
 
+import android.app.NotificationManager;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
@@ -24,53 +25,62 @@ public class Ch6Activity1 extends AppCompatActivity {
         setContentView(R.layout.layout_ch6_1);
         //在java中获取字符串资源
         String content = getString(R.string.hello);
-        Log.i(Ch6Activity1.this.toString(), content);
 
+        Log.i(Ch6Activity1.this.toString(), content);
 
         String sms = getString(R.string.sms);
         String s = String.format(sms, 100, "张三");
         Log.i(Ch6Activity1.this.toString(), s);
 
+        //获取数组资源
         Resources resources = getResources();
-
-        int[] intArray = resources.getIntArray(R.array.intArr);
-        for (int i = 0; i < intArray.length; i++) {
-            Log.i(Ch6Activity1.this.toString(), intArray[i] + "");
+        int[] intArr = resources.getIntArray(R.array.intArr);
+        for (int i = 0; i < intArr.length; i++) {
+            Log.i(Ch6Activity1.this.toString(), intArr[i] + "");
+        }
+        //字符串数组
+        String[] strArr = resources.getStringArray(R.array.strArr);
+        for (int i = 0; i < strArr.length; i++) {
+            Log.i(Ch6Activity1.this.toString(), strArr[i]);
         }
 
-        String[] stringArray = resources.getStringArray(R.array.strArr);
-        for (int i = 0; i < stringArray.length; i++) {
-            Log.i(Ch6Activity1.this.toString(), stringArray[i]);
-        }
-
+        //普通类型数组
         TypedArray typedArray = resources.obtainTypedArray(R.array.commanArr);
         ImageView imageView = (ImageView) findViewById(R.id.ch6_1_iv);
-        int resourceId = typedArray.getResourceId(0, 0);
-        imageView.setImageResource(resourceId);
+        int resId = typedArray.getResourceId(0, 0);
+        imageView.setImageResource(resId);
 
         String str = typedArray.getString(1);
         Log.i(Ch6Activity1.this.toString(), str);
 
+        //注册上下文菜单
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ch6_1_ll);
         registerForContextMenu(linearLayout);
 
         XmlPullParser xmlPullParser = resources.getXml(R.xml.words);
+
         try {
             while (xmlPullParser.getEventType() != XmlPullParser.END_DOCUMENT) {
                 if (xmlPullParser.getEventType() == XmlPullParser.START_TAG) {
-                    String word = xmlPullParser.getAttributeValue(0);
-                    Log.e(Ch6Activity1.this.toString(),word);
+                    //判断一下是否是word元素（words直接路过）
+                    if (xmlPullParser.getName().equals("word")) {
+                        String word = xmlPullParser.getAttributeValue(0);
+                        Log.e(Ch6Activity1.class.toString(), word);
+                    }
                 }
+                xmlPullParser.next();
             }
-
         } catch (Exception e) {
-            Log.e(Ch6Activity1.this.toString(),e.toString());
+            Log.e(Ch6Activity1.class.toString(), e.toString());
         }
-
+        //使用NotificationManager取消通知
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(101);
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        //加载自定义的菜单资源
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.mymenu1, menu);
     }
@@ -81,9 +91,11 @@ public class Ch6Activity1 extends AppCompatActivity {
             case R.id.mymenu_item1:
                 Toast.makeText(this, "item1", Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.mymenu_item2:
                 Toast.makeText(this, "item2", Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.mymenu_item3:
                 Toast.makeText(this, "item3", Toast.LENGTH_SHORT).show();
                 break;
@@ -99,9 +111,11 @@ public class Ch6Activity1 extends AppCompatActivity {
             case R.id.mymenu_item1:
                 Toast.makeText(this, "item1", Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.mymenu_item2:
                 Toast.makeText(this, "item2", Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.mymenu_item3:
                 Toast.makeText(this, "item3", Toast.LENGTH_SHORT).show();
                 break;
@@ -111,6 +125,7 @@ public class Ch6Activity1 extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //加载自定义的菜单资源
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.mymenu1, menu);
         return true;
